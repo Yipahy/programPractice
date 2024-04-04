f00d1ca6ada536035fc251c43d81ffdbb23b843f
yyds <2785135187@qq.com>
2024-04-04 08:22:11 +0800

    day2

--- day1/pointer.c
+++ day1/pointer.c
@@ -6,7 +6,7 @@ void func(char** p){
 	*p="hello world";
 }//p被释放
 void* func1(){
-	char* p="hello";//p指向常量区的hello(浅拷贝)
+	char* p="hello";//p指向data区中的常量区的hello(浅拷贝)
 	return p;
 }
 void* func2(){
@@ -19,13 +19,12 @@ void* func3(){
 	char p[16];
 	strcpy(p,"hello2");//p指向拷贝在栈区的hello2(深拷贝)
 	return p;//返回野指针
-}//p已被释放，栈区hello2释放
+}//p被释放,指向的对应栈区也释放
 void func4(char* p){//即p=buf4
-	p="hello3";//p不再指向hhhhh,而是指向hello3(浅拷贝)
+	p="hello3";//p由指向hhhhh变为hello3(浅拷贝)
 }//p被释放
 void func5(char* p){
-	memset(p,0,10);
-	strcpy(p,"hello4");//p指向拷贝在main栈区的hello4(深拷贝)
+	strcpy(p,"hello4");//p与buf5都指向拷贝在栈区的hello4(深拷贝)
 }
 int main(){
 	char* buf;
@@ -36,7 +35,7 @@ int main(){
     buf3=func3();//buf3指向已释放的栈区
 	char* buf4="hhhhh";
 	func4(buf4);//p的修改与buf4无关
-	char buf5[10];
+	char buf5[10]="lllll";
 	func5(buf5);
 	printf("%s\n",buf);//hello world
 	printf("%s\n",buf1);//hello
